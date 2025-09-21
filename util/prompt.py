33a5030ef7867b8215ae708953655d85b1551dd7
jiajiu123 <1771663559@qq.com>
2025-09-21 16:43:50 +0800

    feat: 添加列表选择器和输入框功能

---
+++ util/prompt.py
@@ -0,0 +1,35 @@
+from noneprompt import ListPrompt, InputPrompt, Choice
+
+
+def list(text: str, choices: dict[str:any], need_name: bool = False) -> str:
+    """
+    创建一个列表选择器，并返回用户选择的选项数据。
+
+    Args:
+        text: 选择器的提示文本
+        choices: 选择项的字典，键为选项名称，值为选项数据
+
+    Returns:
+        用户选择的选项数据
+    """
+    choices = [Choice(name, data=value) for name, value in choices.items()]
+    choice = ListPrompt(text, choices=choices).prompt()
+    data = choice.data
+    name = choice.name
+    if need_name:
+        return [name, data]
+    else:
+        return data
+
+
+def input(text: str) -> str:
+    """
+    创建一个输入框，并返回用户输入的文本。
+
+    Args:
+        text: 输入框的提示文本
+
+    Returns:
+        用户输入的文本
+    """
+    return InputPrompt(text).prompt()
