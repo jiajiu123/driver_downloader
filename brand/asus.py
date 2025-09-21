1360487adcf72a2801a391054fe07f7afae63fb6
jiajiu123 <1771663559@qq.com>
2025-09-21 17:10:50 +0800

    fix: 修正列表选择器函数的类型注解和变量命名

--- brand/asus.py
+++ brand/asus.py
@@ -1,17 +1,18 @@
 from util.request import send
 import json
 import unicodedata
+from typing import Dict, List
 from util.prompt import list
 
 
-def search_model(model):
+def search_model(model: str):
     model_data = json.loads(
         send(
             f"https://odinapi.asus.com.cn/apiv2/SearchSuggestion?SystemCode=asus&WebsiteCode=cn&SearchKey={model}&SearchType=ProductsAll&RowLimit=1000"
         )
     )
     model_data = model_data["Result"][0]["Content"]
-    choices = {}
+    choices: Dict[str, str] = {}
     for i in model_data:
         if i["DataId"] != "99999":
             choices[i["Title"]] = i["DataId"]
@@ -20,7 +21,7 @@ def search_model(model):
     select_driver(model)
 
 
-def select_driver(model_id):
+def select_driver(model_id: str):
     osid = list("选择操作系统", {"Windows 10": 45, "Windows 11": 52})
     data = json.loads(
         send(
@@ -28,26 +29,26 @@ def select_driver(model_id):
         )
     )
     data = data["Result"]["Obj"]
-    driver_list = {}
+    driver_list: Dict[str, List[List[str]]] = {}
     for i in data:
-        driver_list_temp = []
+        driver_list_temp: List[List[str]] = []
         for j in i["Files"]:
             url = j["DownloadUrl"]["China"]
             name = f"{j['Title']} {j['Version']}"
             name = unicodedata.normalize("NFKC", name)
-            driver = [name, url]
+            driver: List[str] = [name, url]
             if "microsoft" not in url:
                 driver_list_temp.append(driver)
         driver_list[i["Name"]] = driver_list_temp
     driver = list("选择驱动类型", driver_list)
-    choices = {}
+    choices: Dict[str, str] = {}
     for i in driver:
         choices[i[0]] = i[1]
     url = list("选择驱动", choices, True)
     download_driver(url[1], url[0])
 
 
-def download_driver(url, name):
+def download_driver(url: str, name: str):
     token = send(
         f"https://cdnta.asus.com.cn/api/v1/Token?filePath={url}&systemCode=asus",
         method="POST",
